375ef82f77a9a1dc7ba97d8c4460122d308d9bb4
andresverjan <andresverjan@hotmail.com>
2020-11-16 16:32:06 -0500

    Creando GENERIC HTTP SERVICE Para DEBUG.

--- src/app/modules/login-admin/login.service.ts
+++ src/app/modules/login-admin/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import * as Globals from "../../modules/core/globals";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { HttpService } from "../core/services/HttpService";
 
 @Injectable({
   providedIn: "root",
@@ -10,7 +11,7 @@ export class LoginService {
   serverUrl: string;
   SERVER_RECURSO_LOGIN_ADMIN = "";
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private httpService: HttpService) {
     this.serverUrl = Globals.SERVER;
   }
 
@@ -47,7 +48,6 @@ export class LoginService {
       }
       `,
     };
-    let headers = new HttpHeaders().set("Content-Type", "application/json");
-    return this.http.post(this.serverUrl, body, { headers: headers });
+    return this.httpService.callApi(body);
   }
 }
