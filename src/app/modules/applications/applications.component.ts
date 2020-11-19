c50612f9c118799bb8643f27e63e0253272928f7
andresverjan <andresverjan@hotmail.com>
2020-11-19 18:22:41 -0500

    Mejoras default values para applicaciones

--- src/app/modules/applications/applications.component.ts
+++ src/app/modules/applications/applications.component.ts
@@ -223,7 +223,24 @@ export class ApplicationsComponent implements OnInit {
   fetchCamposByTabla(obj) {
     this.IsWaiting = true;
     this._columnaService.getAll(obj).subscribe((res) => {
-      this.campos = res.data.listaCamposTable;
+      console.log(res.data.listaCamposTable);
+      //this.campos = res.data.listaCamposTable;      
+
+      this.campos = res.data.listaCamposTable.map( item =>  {        
+        item.nombreUi = item.nombre;
+        item.mascaraId = 1;
+        item.tipoCampoId=1;
+        item.orden= 1;
+        item.minLength= 1;
+        item.maxLength= 255;
+
+        item.requerido = false;
+        item.visible= false;
+        item.buscador= false;
+        item.verList= false;
+        return item;
+      });
+      
       this.IsWaiting = false;
     });
   }
@@ -261,7 +278,7 @@ export class ApplicationsComponent implements OnInit {
       };
       this.aplicacionForm.reset();
       this.campos = new Array<Campo>();
-      Swal.fire("Operación exitosa", "Aplicación agragada!.", "success");
+      Swal.fire("Operación exitosa", "Aplicación guardada correctamente!.", "success");
 
       this.fetchApplications();
 
