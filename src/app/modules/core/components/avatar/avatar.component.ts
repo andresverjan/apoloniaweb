0594a13d15d2c4da9d660cfe85219c142f99acad
andresverjan <andresverjan@hotmail.com>
2020-11-24 00:57:11 -0500

    Mejoras Componentes AVATAR - MULTI_LIST

--- src/app/modules/core/components/avatar/avatar.component.ts
+++ src/app/modules/core/components/avatar/avatar.component.ts
@@ -1,51 +1,46 @@
-import { SafeUrl } from '@angular/platform-browser';
-import { FileHandle } from './avatarDragDropDirective';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { SafeUrl } from "@angular/platform-browser";
+import { FileHandle } from "./avatarDragDropDirective";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-avatar',
-  templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.css']
+  selector: "app-avatar",
+  templateUrl: "./avatar.component.html",
+  styleUrls: ["./avatar.component.css"],
 })
 export class AvatarComponent implements OnInit {
-
   public message: string;
 
-  @Input()
-  editmode = false;
+  @Input() editmode = false;
+  @Input("width") public width: number;
+  @Input() url: string | ArrayBuffer | SafeUrl = "";
+  @Input() marginInput: string = "";  
 
-  @Input('width') public width: number;
-//  @Input('height') public height: number;
-
-  @Input()
-  url: string | ArrayBuffer | SafeUrl = '';
-
-  @Output()
-  urlChange = new EventEmitter();
+  @Output() urlChange = new EventEmitter();
 
   files: FileHandle[] = [];
+  removeAvatar = 'url("/assets/avatar.png");';
 
-  ngOnInit() {
-  }
 
+  ngOnInit() {}
+  
   constructor() {}
 
   filesDropped(files: FileHandle[]): void {
     this.files = files;
-    //console.log('Files: ',  files[0]);
     const fi: File[] = [files[0].file];
     this.preview(fi);
   }
 
   preview(files: File[]) {
-    console.log('files : ', files);
+    this.removeAvatar = "none";
+    console.log("files : ", files);
     if (files.length === 0) {
       return;
     }
-    console.log('file : ', files[0]);
+    console.log("file : ", files[0]);
     const mimeType = files[0].type;
     if (mimeType.match(/image\/*/) == null) {
-      this.message = 'Only images are supported.';
+      this.message = "Only images are supported.";
       return;
     }
 
@@ -55,7 +50,6 @@ export class AvatarComponent implements OnInit {
       this.url = reader.result;
       this.urlChange.emit(this.url);
       this.message = "";
-    }
+    };
   }
-
 }
