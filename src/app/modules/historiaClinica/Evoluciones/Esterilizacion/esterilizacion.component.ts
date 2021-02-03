7a9c3d6911583e37cdb3a89b22a559b6675b1c20
Andres Diaz <andresf.diazm@unac.edu.co>
2021-02-03 15:41:26 -0500

    Arreglo y adicion del boton añadir esterilizacion

---
+++ src/app/modules/historiaClinica/Evoluciones/Esterilizacion/esterilizacion.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, ViewChild, TemplateRef } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { TableService } from "../../../core/services/table.service";
+import { ColumnaService } from "../../../core/services/columna.service";
+import { TipoCampoService } from "../../../tipo-campo/tipo-campo.service";
+import { EsterilizacionService } from "./esterilizacion.service";
+import { OdontologosService } from "../../../core/services/odontologos.service";
+
+@Component({
+  selector: "app-esterilizacion",
+  templateUrl: "./esterilizacion.component.html",
+  styleUrls: ["./esterilizacion.component.scss"],
+})
+export class EsterilizacionComponent implements OnInit {
+  public IsWaiting: boolean;
+  public showListado: boolean = true;
+  public showForm: boolean = false;
+  public dialogRef: any;
+  public etiquetaListado = "Listado de Mascaras";
+  public etiquetaNombreModulo = "Campos";
+  @ViewChild("myDialog") myDialog: TemplateRef<any>;
+
+  constructor(public dialog: MatDialog,) {}
+
+  actionAdicionar() {
+    this.showListado = false;
+    this.showForm = true;
+  }
+  guardar() {
+    this.showForm = false;
+    this.showListado = true;
+  }
+
+  cancelar() {
+    this.showForm = false;
+    this.showListado = true;
+  }
+  closeDialog() {
+    
+    this.dialogRef.close();
+  }
+  openDialogWithTemplateRef(
+    templateRef: TemplateRef<any>,
+  ) {
+    this.dialogRef = this.dialog.open(templateRef, {
+      disableClose: true,
+    });
+    this.dialogRef.afterClosed().subscribe(() => {});
+  }
+
+  openModal(){
+    this.openDialogWithTemplateRef(this.myDialog);
+  }
+  ngOnInit() {}
+}
