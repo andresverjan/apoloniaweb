51ecc1f9a5d63baaaba96c33dab760199b26549e
Luis Alvarez <lealvarezp@unac.edu.co>
2021-02-12 17:59:01 -0500

    funcionalidad inicial

---
+++ src/app/modules/core/components/multilist-observaciones/multilist-observaciones.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "app-multilist-observaciones",
+  templateUrl: "./multilist-observaciones.component.html",
+  styleUrls: ["./multilist-observaciones.component.scss"],
+})
+export class MultilistObservacionesComponent implements OnInit {
+  @Input() sel1: SelItem[]; // input
+  @Input() sel2: SelItem[]; // input/output
+  @Output() emitter = new EventEmitter<SelItem[]>();
+  sel1Filter = "";
+  sel2Filter = "";
+
+  constructor() {
+    if (!this.sel2 || this.sel2.length < 1) {
+      this.sel2 = [];
+    }
+  }
+
+  ngOnInit(): void {}
+
+  getSel1Options() {
+    return this.sel1
+      .filter((s1) => !this.sel2Contains(s1.id))
+      .filter((s1) =>
+        this.sel1Filter
+          ? s1.nombre.toUpperCase().includes(this.sel1Filter.toUpperCase())
+          : true
+      );
+  }
+
+  getSel2Options() {
+    return this.sel2.filter((s2) =>
+      this.sel2Filter
+        ? s2.nombre.toUpperCase().includes(this.sel2Filter.toUpperCase())
+        : true
+    );
+  }
+
+  sel2Contains(id: string) {
+    return this.sel2.some((s2) => s2.id === id);
+  }
+
+  actionAddOnClick(element: SelItem) {
+    this.sel1 = this.sel1.filter((opt) => element.id != opt.id);
+
+    if (this.sel2.indexOf(element) < 0) {
+      this.sel2.push(element);
+    }
+  }
+
+  actionRemoveOnClick(element: SelItem) {
+    this.sel2 = this.sel2.filter((opt) => element.id != opt.id);
+
+    if (this.sel1.indexOf(element) < 0) {
+      this.sel1.push(element);
+    }
+  }
+}
+
+//TODO: Definir modelo de medicamentos
+interface SelItem {
+  id: string;
+  nombre: string;
+}
--- src/app/modules/mascaras/mascaras.component.ts
+++ src/app/modules/mascaras/mascaras.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit, ViewChild, TemplateRef } from "@angular/core";
-import { TableService } from "../core/services/table.service";
-import { ColumnaService } from "../core/services/columna.service";
-import { TipoCampoService } from "../tipo-campo/tipo-campo.service";
 import { MascarasService } from "./mascaras.service";
-import { OdontologosService } from "../core/services/odontologos.service";
 
 @Component({
   selector: "app-mascaras",
@@ -11,31 +7,29 @@ import { OdontologosService } from "../core/services/odontologos.service";
   styleUrls: ["./mascaras.component.scss"],
 })
 export class MascarasComponent implements OnInit {
-  public IsWaiting: boolean;
-  public showListado: boolean = true;
-  public showContent: boolean = true;
-  public showForm: boolean = false;
-  public mascaras = [];
-  public etiquetaListado = "Listado de Mascaras";
-  public etiquetaNombreModulo = "Campos";
+  constructor(public _mascarasService: MascarasService) {}
 
-  constructor(
-    public _mascarasService: MascarasService,
-    public _tipoCampoService: TipoCampoService,
-    public _tableService: TableService,
-    public _columnaService: ColumnaService,
-    public _odontologosService: OdontologosService
-  ) {}
+  SEL1 = [
+    { id: 6, nombre: "Salir" },
+    { id: 7, nombre: "Mascaras" },
+    { id: 8, nombre: "Permisos" },
+    { id: 9, nombre: "Usuarios" },
+    { id: 10, nombre: "Proveedores" },
+    { id: 11, nombre: "Empleados" },
+    { id: 12, nombre: "Rol Permiso" },
+    { id: 37, nombre: "Especialistas" },
+    { id: 38, nombre: "Configuración" },
+    { id: 39, nombre: "Pacientes" },
+    { id: 40, nombre: "Citas" },
+    { id: 41, nombre: "Historia Clinica" },
+  ];
+  SEL2 = [
+    { id: 1, nombre: "Aplicaciones" },
+    { id: 2, nombre: "Generic" },
+    { id: 3, nombre: "Perfil" },
+    { id: 4, nombre: "Rol" },
+    { id: 5, nombre: "Idiomas" },
+  ];
 
-  odontologo: any;
-
-  onOdontologoSelected(selected) {
-    this.odontologo = selected;
-  }
-
-  ngOnInit() {
-    this.odontologo = {
-      nombre: "Seleccionar Especialista",
-    };
-  }
+  ngOnInit() {}
 }
