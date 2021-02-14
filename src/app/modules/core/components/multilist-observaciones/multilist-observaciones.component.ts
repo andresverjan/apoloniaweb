a76d372a1170ecf04112a7c83d5a43673e025989
Luis Alvarez <lealvarezp@unac.edu.co>
2021-02-13 22:03:33 -0500

    modal de observaciones

--- src/app/modules/core/components/multilist-observaciones/multilist-observaciones.component.ts
+++ src/app/modules/core/components/multilist-observaciones/multilist-observaciones.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  TemplateRef,
+  ViewChild,
+} from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { MatMenuTrigger } from "@angular/material/menu";
 
 @Component({
   selector: "app-multilist-observaciones",
@@ -6,25 +16,43 @@ import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
   styleUrls: ["./multilist-observaciones.component.scss"],
 })
 export class MultilistObservacionesComponent implements OnInit {
-  @Input() sel1: SelItem[];
+  @Input() sel1: Item[] = [];
   @Input() nonSelectedItemsTitle: String = "Elementos para seleccionar";
   @Input() selectedItemsTitle: String = "Elementos seleccionados";
   @Input() materialIconName: String = "category";
-  @Input() sel2: SelItem[] = [];
-  @Output() emitter = new EventEmitter<SelItem[]>();
-  sel1Filter = "";
-  sel2Filter = "";
+  @Input() sel2: Item[] = [];
+  @Output() emitter = new EventEmitter<Item[]>();
 
-  constructor() {}
+  @ViewChild(MatMenuTrigger, { static: true }) matMenuTrigger: MatMenuTrigger;
+  @ViewChild("observacionesDialog") myDialog: TemplateRef<any>;
+  public sel1Filter = "";
+  public sel2Filter = "";
+  public dialogRef: any;
+  public selected: Item;
+
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  openDialogWithTemplateRef(templateRef: TemplateRef<any>) {
+    this.dialogRef = this.dialog.open(templateRef, {});
+
+    this.dialogRef.afterClosed().subscribe((result) => result);
+  }
+  closeDialog() {
+    this.dialogRef.close();
+  }
+  save() {
+    console.log(this.selected);
+    this.dialogRef.close();
+  }
 
   getSel1Options() {
     return this.sel1
       .filter((s1) => !this.sel2Contains(s1.id))
       .filter((s1) =>
         this.sel1Filter
-          ? s1.nombre.toUpperCase().includes(this.sel1Filter.toUpperCase())
+          ? s1.name.toUpperCase().includes(this.sel1Filter.toUpperCase())
           : true
       );
   }
@@ -32,7 +60,7 @@ export class MultilistObservacionesComponent implements OnInit {
   getSel2Options() {
     return this.sel2.filter((s2) =>
       this.sel2Filter
-        ? s2.nombre.toUpperCase().includes(this.sel2Filter.toUpperCase())
+        ? s2.name.toUpperCase().includes(this.sel2Filter.toUpperCase())
         : true
     );
   }
@@ -41,15 +69,26 @@ export class MultilistObservacionesComponent implements OnInit {
     return this.sel2.some((s2) => s2.id === id);
   }
 
-  actionAddOnClick(element: SelItem) {
-    this.sel1 = this.sel1.filter((opt) => element.id != opt.id);
+  actionAddOnClick(element: Item) {
+    console.log(element);
+    element.observaciones &&
+    element.observaciones.length > 0 &&
+    element.observaciones !== ""
+      ? element.observaciones
+      : (element.observaciones = "");
 
-    if (this.sel2.indexOf(element) < 0) {
-      this.sel2.push(element);
-    }
+    this.selected = element;
+
+    this.openDialogWithTemplateRef(this.myDialog);
+
+    // this.sel1 = this.sel1.filter((opt) => element.id != opt.id);
+
+    // if (this.sel2.indexOf(element) < 0) {
+    //   this.sel2.push(element);
+    // }
   }
 
-  actionRemoveOnClick(element: SelItem) {
+  actionRemoveOnClick(element: Item) {
     this.sel2 = this.sel2.filter((opt) => element.id != opt.id);
 
     if (this.sel1.indexOf(element) < 0) {
@@ -59,7 +98,8 @@ export class MultilistObservacionesComponent implements OnInit {
 }
 
 //TODO: Definir modelo de medicamentos
-interface SelItem {
+interface Item {
   id: string;
-  nombre: string;
+  name: string;
+  observaciones: string;
 }
--- src/app/modules/mascaras/mascaras.component.ts
+++ src/app/modules/mascaras/mascaras.component.ts
@@ -10,25 +10,25 @@ export class MascarasComponent implements OnInit {
   constructor(public _mascarasService: MascarasService) {}
 
   SEL1 = [
-    { id: 6, nombre: "Salir" },
-    { id: 7, nombre: "Mascaras" },
-    { id: 8, nombre: "Permisos" },
-    { id: 9, nombre: "Usuarios" },
-    { id: 10, nombre: "Proveedores" },
-    { id: 11, nombre: "Empleados" },
-    { id: 12, nombre: "Rol Permiso" },
-    { id: 37, nombre: "Especialistas" },
-    { id: 38, nombre: "Configuración" },
-    { id: 39, nombre: "Pacientes" },
-    { id: 40, nombre: "Citas" },
-    { id: 41, nombre: "Historia Clinica" },
+    { id: 6, name: "Salir" },
+    { id: 7, name: "Mascaras" },
+    { id: 8, name: "Permisos" },
+    { id: 9, name: "Usuarios" },
+    { id: 10, name: "Proveedores" },
+    { id: 11, name: "Empleados" },
+    { id: 12, name: "Rol Permiso" },
+    { id: 37, name: "Especialistas" },
+    { id: 38, name: "Configuración" },
+    { id: 39, name: "Pacientes" },
+    { id: 40, name: "Citas" },
+    { id: 41, name: "Historia Clinica" },
   ];
   SEL2 = [
-    { id: 1, nombre: "Aplicaciones" },
-    { id: 2, nombre: "Generic" },
-    { id: 3, nombre: "Perfil" },
-    { id: 4, nombre: "Rol" },
-    { id: 5, nombre: "Idiomas" },
+    { id: 1, name: "Aplicaciones" },
+    { id: 2, name: "Generic" },
+    { id: 3, name: "Perfil" },
+    { id: 4, name: "Rol" },
+    { id: 5, name: "Idiomas" },
   ];
 
   ngOnInit() {}
