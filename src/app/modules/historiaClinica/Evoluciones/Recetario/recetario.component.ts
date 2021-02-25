73dd08e4504d631fefc22923143b082d8853460d
Luis Alvarez <lealvarezp@unac.edu.co>
2021-02-24 20:49:21 -0500

    listing and saving on global array

--- src/app/modules/historiaClinica/Evoluciones/Recetario/recetario.component.ts
+++ src/app/modules/historiaClinica/Evoluciones/Recetario/recetario.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 import { MatDialog } from "@angular/material/dialog";
 import { OdontologosService } from "src/app/modules/core/services/odontologos.service";
+import { RecetarioService } from "./recetario.service";
 
 @Component({
   selector: "app-recetario",
   templateUrl: "./recetario.component.html",
-  styleUrls: ["./recetario.component.sass"],
+  styleUrls: ["./recetario.component.scss"],
 })
 export class RecetarioComponent implements OnInit {
-  public IsWaiting: boolean;
+  public recetarioForm: FormGroup;
+  public isWaiting: boolean = true;
   public showListado: boolean = true;
   public showForm: boolean = false;
   public dialogRef: any;
   public etiquetaListado = "Listado de Mascaras";
   public etiquetaNombreModulo = "Campos";
   public odontologo: any;
+  public recetario: Array<any> = [];
 
   SEL1 = [
     { id: 6, name: "Salir" },
@@ -41,13 +45,16 @@ export class RecetarioComponent implements OnInit {
 
   constructor(
     public dialog: MatDialog,
-    public _odontologosService: OdontologosService
+    public _odontologosService: OdontologosService,
+    public recetarioService: RecetarioService
   ) {
     this.odontologo = {
       Nombres: "Seleccionar Especialista",
     };
   }
-
+  ngOnInit() {
+    this.fetch();
+  }
   actionAdicionar() {
     this.showListado = false;
     this.showForm = true;
@@ -55,8 +62,9 @@ export class RecetarioComponent implements OnInit {
   guardar() {
     this.showForm = false;
     this.showListado = true;
+    this.recetario.push(); //TODO: push new object to global array
   }
-
+  actualizar(item) {}
   cancelar() {
     this.showForm = false;
     this.showListado = true;
@@ -64,22 +72,30 @@ export class RecetarioComponent implements OnInit {
   closeDialog() {
     this.dialogRef.close();
   }
+  onSelection(e) {
+    console.log(e);
+  }
   openDialogWithTemplateRef(templateRef: TemplateRef<any>) {
     this.dialogRef = this.dialog.open(templateRef, {
       height: "810px",
       width: "650px",
       disableClose: true,
     });
-    this.dialogRef.afterClosed().subscribe(() => {});
+    this.dialogRef.afterClosed().subscribe((a) => {});
   }
-
   openModal() {
     this.openDialogWithTemplateRef(this.myDialog);
   }
-  ngOnInit() {}
-
   onOdontologoSelected(selected) {
+    this.isWaiting = true;
     this.odontologo = selected;
-    this.IsWaiting = true;
+    this.isWaiting = false;
+  }
+  fetch() {
+    this.isWaiting = true;
+    this.recetarioService.getAll().subscribe(({ data }) => {
+      this.recetario = data.mascaras; //TODO: mascaras -> recetario
+      this.isWaiting = false;
+    });
   }
 }
