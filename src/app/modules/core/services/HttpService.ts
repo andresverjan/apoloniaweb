222c159d996a6da249c5fc37adbcd1e54720bae7
juvenalelias <juvenalelias@gmail.com>
2021-04-27 20:59:25 -0500

    ajustando environment frontend

--- src/app/modules/core/services/HttpService.ts
+++ src/app/modules/core/services/HttpService.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import * as Globals from "../globals";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
@@ -16,8 +17,9 @@ export class HttpService {
  public callApi(body): Observable<any> {
     console.log("*************************");
     console.log(body.query.toString().replace(/\r?\n/g, "").replace(/\s+/g, " "));
-    console.log("*************************");
+    console.log("***********-environment.apiUrl-**************", environment.apiUrl);
     let headers = new HttpHeaders().set("Content-Type", "application/json");
-    return this.http.post(this.serverUrl, body, { headers: headers });
+//    return this.http.post(this.serverUrl, body, { headers: headers });
+    return this.http.post(environment.apiUrl, body, { headers: headers });
   }
 }
