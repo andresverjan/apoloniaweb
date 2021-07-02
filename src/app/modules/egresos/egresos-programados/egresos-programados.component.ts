c49f333b557162812260fe503a68d18f5524bc3b
Luis Alvarez <lealvarezp@unac.edu.co>
2021-07-02 04:00:24 -0500

    avances front egresos programados

--- src/app/modules/egresos/egresos-programados/egresos-programados.component.ts
+++ src/app/modules/egresos/egresos-programados/egresos-programados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-egresos-programados",
@@ -8,9 +9,24 @@ import { Component, OnInit } from "@angular/core";
 export class EgresosProgramadosComponent implements OnInit {
   public IsWaiting: boolean;
   public showListado: boolean = true;
-  public showForm: boolean = false;
+  public egresosProgramadosForm: FormGroup;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.egresosProgramadosForm = new FormGroup({
+      T17Factura: new FormControl(""),
+      T17Proveedor: new FormControl(""),
+      T17Soporte: new FormControl(""),
+      T17Valor: new FormControl(0),
+      T17Dctos: new FormControl(0),
+      T17IVA: new FormControl(0),
+      T17Fecha: new FormControl(""),
+      T17ICA: new FormControl(0),
+      T17Total: new FormControl(""),
+      T17FormaPago: new FormControl(""),
+      T17RF: new FormControl(0),
+      T17Observacion: new FormControl(""),
+    });
+  }
 }
