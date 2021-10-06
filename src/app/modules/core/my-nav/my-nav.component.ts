267c01ec3d07bd4bf059f00448b4cbff72614bf1
harold.andres.verjan.diaz <andresverjan@hotmail.com>
2021-10-06 10:38:56 -0500

    Mejoras Menu Navegacion

--- src/app/modules/core/my-nav/my-nav.component.ts
+++ src/app/modules/core/my-nav/my-nav.component.ts
@@ -14,6 +14,7 @@ import { MatMenuTrigger } from "@angular/material/menu";
 })
 export class MyNavComponent {  
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+  @ViewChild('childMenu') public childMenu;
   
   public session: UserSession;
   public serverURlImagesUsuarios: String;
