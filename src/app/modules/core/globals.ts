568a572c2a608d9e16f183a17fd6c12a705201c0
Harold Andres Verjan Diaz <andresverjan@hotmail.com>
2021-10-08 18:57:01 -0500

    update globals. Azure.

--- src/app/modules/core/globals.ts
+++ src/app/modules/core/globals.ts
@@ -12,9 +12,9 @@ export const DEFAULT_LANGUAGE = "1";
  */
 export const SERVER_DEV = "http://localhost:3000/api";
 export const SERVER_PROD = "https://apoloniaoe.herokuapp.com/api";
-export const SERVER_PROD_IP = "https://apoloniaoe.herokuapp.com/api";
+export const SERVER_PROD_IP = "https://apoloniabackend.azurewebsites.net/api";
 
-export const SERVER = SERVER_PROD;
+export const SERVER = SERVER_PROD_IP;
 
 /*
  *  Definicion de constantes de providers Google facebook
