31263b0c36fdc9db93a625c3c819915e9aecfcda
Harold Andres Verjan Diaz <andresverjan@hotmail.com>
2021-10-14 02:14:50 -0500

    Update Evoluciones

--- src/app/modules/historiaClinica/Evoluciones/evoluciones.service.ts
+++ src/app/modules/historiaClinica/Evoluciones/evoluciones.service.ts
@@ -3,13 +3,14 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { ToolsService } from "../../core/services/tools.service";
 import { environment } from "src/environments/environment";
+import { HttpService } from "../../core/services/HttpService";
 
 @Injectable({
   providedIn: "root",
 })
 export class EvolucionesService {
 
-  constructor(private http: HttpClient, private toolService: ToolsService) {}
+  constructor(private http: HttpClient, private toolService: ToolsService, private httpService: HttpService) { }
 
   getAll(objeTosend?: any): Observable<any> {
     let params = "";
@@ -27,10 +28,14 @@ export class EvolucionesService {
     let body = {
       query: `{
         getCitasHC ${filter}{
+          id
           Paciente
           Cedula
           IdOdontologo
           Fecha
+          Observacion
+          Nombres
+          Apellidos
         }
       }
       `,
@@ -39,4 +44,49 @@ export class EvolucionesService {
     let headers = new HttpHeaders().set("Content-Type", "application/json");
     return this.http.post(environment.apiUrl, body, { headers: headers });
   }
+
+  save(objeTosend): Observable<any> {
+    let paramTest = [];
+    Object.entries(objeTosend).forEach(([key, value]) => {
+      paramTest.push(key + ': "' + value + '"');
+    });
+    console.log(paramTest);
+    console.log(paramTest.toString());
+    let body = {
+      query: `
+          mutation {
+            saveEvolucion (citaHC: {${paramTest.toString()}}   ) { 
+                title
+            }
+          }
+          `,
+    };
+    return this.httpService.callApi(body);
+  }
+
+  update(objeTosend): Observable<any> {
+    let paramTest = [];
+    Object.entries(objeTosend).forEach(([key, value]) => {
+      if(key === 'id'){
+        paramTest.push(key + ': ' + value );
+      }else{
+        paramTest.push(key + ': "' + value + '"');  
+      }
+    });
+    console.log(paramTest);
+    console.log(paramTest.toString());
+    let body = {
+      query: `
+          mutation {
+            updateEvolucion (citaHC: {${paramTest.toString()}}   ) { 
+                id
+                Cedula
+            }
+          }
+          `,
+    };
+    return this.httpService.callApi(body);
+  }
+
+
 }
