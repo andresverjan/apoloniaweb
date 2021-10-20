31602f276b85a431fc6e20c072540bf41ef3f955
Harold Andres Verjan Diaz <andresverjan@hotmail.com>
2021-10-19 22:55:44 -0500

    Creating Basic component

---
+++ src/app/modules/core/components/basiccard/basiccard.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "app-basiccard",
+  templateUrl: "./basiccard.component.html",
+  styleUrls: ["./basiccard.component.scss"],
+})
+export class BasiccardComponent implements OnInit {
+  @Input() data: any;
+  @Input() label: string;
+  @Input() icono: string= "account_balance";
+  @Input() bgColor: string;
+  @Input() fontColor: string;
+  @Input() valor: string;
+  @Input() size: string;
+  @Input() default: string;
+  @Output() valSelected = new EventEmitter<string>();
+  @Input() onClickFunction: (args?: any) => void;
+
+
+  constructor() {
+
+  }
+  
+  ngOnInit() {
+    console.log(this.bgColor);
+    /*const exist = this.data.some((r: { value: string }) => r.value == "");
+    if (!exist) {
+      this.data.push({ value: "", nombre: "All" });
+    }*/
+  }
+
+  runOnChange(valSelect: any) {
+    this.valSelected.emit(valSelect);
+  }
+
+  onClick(){
+    if ( typeof(this.onClickFunction)=="function"){
+      this.onClickFunction(this.label);
+    }
+  }  
+}
