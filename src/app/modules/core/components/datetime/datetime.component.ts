9213727ec1a96dd726b019487a7e8798aa1054c3
HAROLD VERJAN <andresverjandiaz@gmail.com>
2022-01-25 03:05:58 -0500

    Mejoras pequeñas date and datetime
    Mejoras pequeñas date and datetime

--- src/app/modules/core/components/datetime/datetime.component.ts
+++ src/app/modules/core/components/datetime/datetime.component.ts
@@ -1,79 +1,75 @@
 import {
-  Component,
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  EventEmitter,
-  Input,
-  Output,
+    Component,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    EventEmitter,
+    Input,
+    Output
 } from "@angular/core";
-import {
-  DateAdapter,
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE,
-} from "@angular/material/core";
-import {
-  MomentDateAdapter,
-  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
-} from "@angular/material-moment-adapter";
-import { MatDatepickerInputEvent } from "@angular/material/datepicker";
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from "@angular/material/core";
+import {MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from "@angular/material-moment-adapter";
 import * as moment from "moment";
-import {
-  AppDateAdapter,
-  APP_DATETIME_FORMATS,
-} from "src/app/modules/core/components/datepicker/format-datepicker";
+import {APP_DATETIME_FORMATS} from "src/app/modules/core/components/datepicker/format-datepicker";
 
 @Component({
-  selector: "app-datetime",
-  templateUrl: "./datetime.component.html",
-  styleUrls: ["./datetime.component.scss"],
-  providers: [
-    { provide: DateAdapter, useClass: MomentDateAdapter },
-    {
-      provide: MAT_DATE_FORMATS,
-      useValue: APP_DATETIME_FORMATS,
-      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
-    },
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: "app-datetime",
+    templateUrl: "./datetime.component.html",
+    styleUrls: ["./datetime.component.scss"],
+    providers: [
+        {
+            provide: DateAdapter,
+            useClass: MomentDateAdapter
+        }, {
+            provide: MAT_DATE_FORMATS,
+            useValue: APP_DATETIME_FORMATS,
+            deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+        },
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DatetimeComponent {
-  private onChange = (value: any) => { };
-  @Input() label: any;
-  @Input() dateValue: string = null;
-  @Input() requerido: boolean = false;
-  @Output() valor = new EventEmitter<string>();
+    private onChange = (value : any) => {};
+    @Input()label : any;
+    @Input()dateValue : string = null;
+    @Input()requerido : boolean = false;
+    @Output()valor = new EventEmitter<string>();
 
-  public parms: any = {
-    disabled: false,
-    showSpinners: true,
-    showSeconds: false,
-    disableMinute: false,
-    touchUi: false,
-    hideTime: false,
-    enableMeridian: false,
-    stepHour: 1,
-    stepMinute: 1,
-    stepSecond: 1,
-    color: "Primary",
-  };
+    public parms : any = {
+        disabled: false,
+        showSpinners: true,
+        showSeconds: false,
+        disableMinute: false,
+        touchUi: false,
+        hideTime: false,
+        enableMeridian: false,
+        stepHour: 1,
+        stepMinute: 1,
+        stepSecond: 1,
+        color: "Primary"
+    };
 
-  constructor(private cdr: ChangeDetectorRef) { }
-
-  ngOnInit(): void { }
-
-  onDateChange(event: any) {
-    this.dateValue = moment(event.value).format();
-    this.onChange(event.value);
-    this.valor.emit(this.dateValue);
-  }
-
-  writeValue(value: any): void {
-    if (value !== undefined || value !== "") {
-      this.dateValue = moment(value).format();
+    constructor(private cdr : ChangeDetectorRef) {}
+    ngOnInit(): void {}
+    onDateChange(event : any) {
+        console.log("onDateChange");        
+        this.dateValue = moment(event.value).format();
+        this.onChange(event.value);
+        console.log(this.dateValue);
+        this.valor.emit(this.dateValue);
     }
-  }
 
-  registerOnChange(fn) {
-    this.onChange = fn;
-  }
+    changeInput(){
+        console.log("entro a changeInput");
+    }
+
+    onDateChangeTest(event : any) {
+        console.log("onDateChangeTest");        
+        this.dateValue = moment(event.value).format();
+        this.onChange(event.value);
+        console.log(this.dateValue);
+        this.valor.emit(this.dateValue);
+        if(this.requerido == true && this.dateValue!= ''){
+            console.log("Son requeridos...");
+        }
+    }
 }
