688eb32782c5b3d633c2c18a1ccb7b2b8787f1bc
GiancarloCabrera <giancarlo.cabrera@unac.edu.co>
2022-03-22 22:00:05 -0500

    2dp avance componente

--- src/app/modules/giancarlo-learning/giancarlo-learning.component.ts
+++ src/app/modules/giancarlo-learning/giancarlo-learning.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { GiancarloLearningService } from './giancarlo-learning.service';
 
 @Component({
@@ -7,10 +8,35 @@ import { GiancarloLearningService } from './giancarlo-learning.service';
   styleUrls: ['./giancarlo-learning.component.scss']
 })
 export class GiancarloLearningComponent implements OnInit {
-
+  lista = [];
+  buttonFlag = false;
+  public Formulario = new FormGroup({
+    id: new FormControl('',Validators.required),
+    nombre: new FormControl('',Validators.required),
+    apellido: new FormControl('',Validators.required),
+    cedula: new FormControl('',Validators.required),
+    email: new FormControl('',Validators.required),
+    fechaNacimiento: new FormControl('',Validators.required),
+    activo: new FormControl(undefined,Validators.required),
+    eliminado: new FormControl(undefined,Validators.required),
+    sexo: new FormControl('',Validators.required),
+    edad: new FormControl(undefined,Validators.required),
+    mascotaFavorita: new FormControl('',Validators.required)
+  })
   constructor(private giancarloLearningService: GiancarloLearningService) { }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  onclick(){
+    this.buttonFlag=true;
+  }
+
+  getData(obj?){
+    this.giancarloLearningService.list(obj).subscribe((response) => {
+      this.lista = response.data.giancarloLearning
+    })
   }
 
 }
--- src/app/modules/giancarlo-learning/giancarlo-learning.service.ts
+++ src/app/modules/giancarlo-learning/giancarlo-learning.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import * as Globals from "../core/globals";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { HttpService } from "../core/services/HttpService";
 
@@ -14,8 +14,26 @@ export class GiancarloLearningService{
         this.serverUrl = Globals.SERVER;
     }
 
-    listar(){
-        
+    list(obj?):  Observable<any>{
+        let body = {
+            query: `{
+                giancarloLearning{
+                    id
+                    nombre
+                    apellido
+                    cedula
+                    email
+                    fechaNacimiento
+                    activo
+                    eliminado
+                    sexo
+                    edad
+                    mascotaFavorita
+                  }
+            }`
+        }
+        let headers = new HttpHeaders().set("Content-Type", "application/json");
+        return this.http.post('https://http://localhost:3000/api', body, { headers: headers});
     }
 
 }
