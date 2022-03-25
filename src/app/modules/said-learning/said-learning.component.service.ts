de6b2132bc25980c39e3df433988c61f453363f6
Said Salcedo <said.salcedoc@unac.edu.co>
2022-03-25 00:33:17 -0500

    Avance front-Said

--- src/app/modules/said-learning/said-learning.component.service.ts
+++ src/app/modules/said-learning/said-learning.component.service.ts
@@ -3,7 +3,6 @@ import * as Globals from "../core/globals";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { HttpService } from "../core/services/HttpService";
-import * as moment from "moment";
 
 @Injectable({
   providedIn: "root",
@@ -11,14 +10,13 @@ import * as moment from "moment";
 export class saidLearningService {
   serverUrl: string;
 
-  constructor(
-    private httpService: HttpService) {
+  constructor(private http: HttpClient,private httpService: HttpService) {
     this.serverUrl = Globals.SERVER;
   }
 
-  getAll(object?):  Observable<any>{
+  getAll(object?): Observable<any> {
     let body = {
-        query: `{
+      query: `{
             saidLearning{
               id
               nombreAnimal
@@ -28,11 +26,60 @@ export class saidLearningService {
               email
               sectorVivienda
               fechaNacimiento
-              activo
-              eliminado
             }
-          }`
-    }
+          }`,
+    };
     return this.httpService.callApi(body);
-}
+  }
+
+  createMascota(mascota): Observable<any> {
+    let body = {
+      query: `
+      mutation {
+        createSaid (saidLearning: {       
+            id:"${mascota.id}",
+            nombreAnimal:"${mascota.nombreAnimal}",
+            nombreDueno:"${mascota.nombreDueno}",
+            Raza:"${mascota.Raza}",
+            NumIdentificacion:"${mascota.NumIdentificacion}",
+            email:"${mascota.email}",
+            sectorVivienda:"${mascota.sectorVivienda}",
+            fechaNacimiento:"${mascota.fechaNacimiento}"
+        })
+      }
+      `,
+    };
+    return this.httpService.callApi(body);
+  }
+
+  updateMascota(mascota): Observable<any> {
+    let body = {
+      query: `
+      mutation {
+        updateSaid (saidLearning: {
+            id:"${mascota.id}",
+            nombreAnimal:"${mascota.nombreAnimal}",
+            nombreDueno:"${mascota.nombreDueno}",
+            Raza:"${mascota.Raza}",
+            NumIdentificacion:"${mascota.NumIdentificacion}",
+            email:"${mascota.email}",
+            sectorVivienda:"${mascota.sectorVivienda}",
+            fechaNacimiento:"${mascota.fechaNacimiento}"
+        })
+      }
+      `,
+    };
+    return this.httpService.callApi(body);
+  }
+
+  deleteMascota(mascotaId): Observable<any> {
+    let body = {
+      query: `
+        mutation {
+          deleteSaid (saidLearning: {id: "${mascotaId.id}"})
+        }
+        `,
+    };
+    return this.httpService.callApi(body);
+  }
 }
--- src/app/modules/said-learning/said-learning.component.ts
+++ src/app/modules/said-learning/said-learning.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import Swal from "sweetalert2";
 import { saidLearningService } from "./said-learning.component.service";
 
 @Component({
@@ -7,14 +9,101 @@ import { saidLearningService } from "./said-learning.component.service";
   styleUrls: ["./said-learning.component.sass"],
 })
 export class SaidLearningComponent implements OnInit {
+  public radioButtonFlag = false;
+  public IsWait: Boolean = false;
+  public lShowPanelListado: Boolean = true;
+  public lShowPanelDatos: Boolean = false;
+  public lShowBtnAdicionar: Boolean = true;
+  public lShowBtnActualizar: Boolean = true;
+  public lShowBtnEliminar: Boolean = true;
   mascota: any = [];
+  public title = "Mascotas";
 
   constructor(private saidLearning: saidLearningService) {}
 
-  ngOnInit(): void {
-    this.saidLearning.getAll().subscribe((saidLearning) => {
-      this.mascota = saidLearning.data.saidLearning;
+  public mascotaForm = new FormGroup({
+    id: new FormControl("", Validators.required),
+    nombreAnimal: new FormControl("", Validators.required),
+    nombreDueno: new FormControl("", Validators.required),
+    Raza: new FormControl("", Validators.required),
+    NumIdentificacion: new FormControl("", Validators.required),
+    email: new FormControl("", Validators.required),
+    sectorVivienda: new FormControl("", Validators.required),
+    fechaNacimiento: new FormControl("", Validators.required),
+  });
+
+  ngOnInit(): void {}
+
+  getData(obj?) {
+    this.saidLearning.getAll(obj).subscribe((response) => {
+      this.mascota = response.data.saidLearning;
     });
   }
-  
+
+  OnDate(valor) {
+    this.mascotaForm.controls["fechaNacimiento"].setValue(valor);
+  }
+
+  verDetalle(dataInput: any) {
+    this.lShowPanelListado = false;
+    this.lShowPanelDatos = true;
+    this.lShowBtnActualizar = true;
+    this.lShowBtnEliminar = true;
+    this.lShowBtnAdicionar = false;
+    this.mascotaForm.patchValue(dataInput);
+  }
+
+  adicionar() {
+    this.lShowPanelListado = false;
+    this.lShowPanelDatos = true;
+    this.mascotaForm.reset();
+    this.lShowBtnActualizar = false;
+    this.lShowBtnEliminar = false;
+    this.lShowBtnAdicionar = true;
+  }
+
+  guardar() {
+    this.IsWait = true;
+    this.saidLearning
+      .createMascota(this.mascotaForm.value)
+      .subscribe((reponse) => {
+        this.IsWait = false;
+        Swal.fire("Mascota", "Agregada correctamente.", "success");
+        // this.findBy();
+        this.mascotaForm.reset();
+        this.lShowPanelDatos = false;
+        this.lShowPanelListado = true;
+      });
+  }
+
+  actualizar() {
+    this.IsWait = true;
+    this.saidLearning.updateMascota(this.mascotaForm.value).subscribe(() => {
+      this.IsWait = false;
+      Swal.fire("Mascota", "Actualizada correctamente.", "success");
+      this.mascotaForm.reset();
+      this.lShowPanelDatos = false;
+      this.lShowPanelListado = true;
+      // this.findBy();
+    });
+  }
+
+  eliminar() {
+    let item = this.mascotaForm.value;
+    this.IsWait = true;
+    this.saidLearning.deleteMascota(item.id).subscribe((reponse) => {
+      this.IsWait = false;
+      Swal.fire("Mascota", "Eliminada correctamente.", "success");
+      this.getData();
+      this.mascotaForm.reset();
+      this.lShowPanelDatos = false;
+      this.lShowPanelListado = true;
+    });
+  }
+
+  cancelar() {
+    this.lShowPanelListado = true;
+    this.lShowPanelDatos = false;
+    this.mascotaForm.reset();
+  }
 }
