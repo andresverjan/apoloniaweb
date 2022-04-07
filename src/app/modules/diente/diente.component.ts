1676529a31f2b9f2763adfd5fe1c8247ff40de80
GiancarloCabrera <giancarlo.cabrera@unac.edu.co>
2022-04-07 11:07:31 -0500

    Avance componente diente

--- src/app/modules/diente/diente.component.ts
+++ src/app/modules/diente/diente.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input  } from '@angular/core';
 
 @Component({
   selector: 'app-diente',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diente.component.scss']
 })
 export class DienteComponent implements OnInit {
-
+  @Input() url: string = "https://cdn.pixabay.com/photo/2017/11/10/05/24/add-2935429_960_720.png";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  onSelectedFile(s){
+    if(s.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(s.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.url=event.target.result;
+      }
+    }
   }
 
 }
